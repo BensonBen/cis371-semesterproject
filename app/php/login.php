0c5e596c5dd4688e1c7d0d4ef994ca0408edba24
zelaskoj <zelaskoj@mail.gvsu.edu>
2016-11-04 14:43:04 -0400

    Added a login php file to be tested later. Also fixed the contributors file. :)

---
+++ app/php/login.php
@@ -0,0 +1,17 @@
+<?php 
+	$con = mysql_connect("cis.gvsu.edu","zelaskoj","zelaskoj1835");
+	if (!$con){
+		die('Could not connect: '. mysql_error());
+	}
+	mysql_selectdb("zelaskoj", $con);
+	$ret = "";
+	$qry = mysql_query("SELECT username FROM cis371_login WHERE username =".$_POST["username"].";");
+	if ($qry == ""){
+		$ret = "Username not found!";
+	else if($_POST["password"] != mysql_query("SELECT password FROM cis371_login WHERE 
+		username =".$_POST["username"].";") {
+		$ret = "Password does not match!";
+		}
+	else $ret = "Login successful";
+	echo $ret;
+?>
