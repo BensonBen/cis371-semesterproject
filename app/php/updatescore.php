6846032233fd79aa6561147cca88be8c0830d8b3
zelaskoj <zelaskoj@mail.gvsu.edu>
2016-12-08 17:09:37 -0500

    Update updatescore.php

--- app/php/updatescore.php
+++ app/php/updatescore.php
@@ -1,5 +1,10 @@
 <html>
+	<head>
+		<title>Menu</title>
+		<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	</head>
 	<body>
+		
 		<?php
 	
 			$con = mysqli_connect("cis.gvsu.edu","zelaskoj","zelaskoj1835", "zelaskoj");
@@ -13,13 +18,15 @@
 			$sql = "INSERT INTO highscores (name, score) VALUES ('$name' , '$score')";
       
       		if($con->query($sql) === TRUE) {
-    			echo "New record created successfully.\n";
+    			echo "Thanks for playing!";
       		}
     			else{echo "Adding failed!";}
       
       		$con->close();
 		?>
-		<a href="../index.html">Play Again</a>
-		<a href="displayScores.php">View High Scores</a>
+		<div class ="row center-text">
+			<a class="btn btn-success" href="../index.html"><span class="glyphicon glyphicon-arrow-left" ></span>&nbsp; Return to Game</a>
+       		 	<a class="btn btn-success" href="php/displayScores.php"> View High Scores<span class="glyphicon glyphicon-arrow-right"></span></a>
+    		</div>
 	</body>
 </html>
