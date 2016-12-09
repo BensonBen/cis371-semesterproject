ee9dba0a381756057a25038368a0597fc47b8c90
zelaskoj <zelaskoj@mail.gvsu.edu>
2016-12-09 09:31:05 -0500

    Update updatescore.php

--- app/php/updatescore.php
+++ app/php/updatescore.php
@@ -20,13 +20,13 @@
 			$sql = "INSERT INTO highscores (name, score, accuracy) VALUES ('$name' , '$score', '$accuracy')";
       
       		if($con->query($sql) === TRUE) {
-    			echo "Thanks for playing!";
       		}
     			else{echo "Adding failed!";}
       
       		$con->close();
 		?>
 		<div class ="row center-text">
+			<p>Thanks for playing!</p>
 			<a class="btn btn-success" href="../index.html"><span class="glyphicon glyphicon-arrow-left" ></span>&nbsp; Return to Game</a>
        		 	<a class="btn btn-success" href="displayScores.php"> View High Scores<span class="glyphicon glyphicon-arrow-right"></span></a>
     		</div>
